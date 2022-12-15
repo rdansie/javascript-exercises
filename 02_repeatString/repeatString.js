de8e514073ed5681e8918319ef72f478bcb6549d
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-15 18:18:56 +0100

    Completed exercise #2 repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let total = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 1; i <= num; i++) {
+        total += string;
+    }
+    return total;
 };
 
 // Do not edit below this line
