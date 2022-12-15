e6463db2915c5b522bd1b8fa234fa05d7f218181
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-15 18:28:10 +0100

    Completed exercise #3 reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let myArray = string.split("");
+    myArray.reverse();
+    myArray.toString();
+    return myArray.join('');
 };
 
 // Do not edit below this line
