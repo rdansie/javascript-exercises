4d629390e7584283d00dc17bb5ee22679cffc558
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-19 18:28:15 +0100

    Completed exercise #6 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) return true;
+    if (year % 100 === 0) return false;
+    if (year % 4 === 0) return true;
+    else return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
+//if input is divisible by 400 return true
+    //if divisible by 100 return false
+        //if divisible by 4 return true
+        //else false
