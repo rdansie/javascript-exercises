57f6a1811a64ed63cf3a58fc91ed53565221fbf8
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-19 15:57:05 +0100

    Some changes to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    //loop through elements of Array
+    for (element of array) {
+        //if it matches the function argument, delete it from the array
+        if (element === num) {
+            let index = array.indexOf(element);
+            array.splice(index,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//turn arguments into array using Array.from()
+
+
+//then run previous function through all elements of arguments array except the 1st
