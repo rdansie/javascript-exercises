e69f1f093e1795fb629f8d4fc872b2cca875d832
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-19 16:49:33 +0100

    Completed exercise #4 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,21 @@
-const removeFromArray = function(array, num) {
-    //loop through elements of Array
-    for (element of array) {
-        //if it matches the function argument, delete it from the array
-        if (element === num) {
-            let index = array.indexOf(element);
-            array.splice(index,1);
+const removeFromArray = function(array) {
+    //Take arguments without the first array
+    const newArg = [].slice.call(arguments, 1);
+    //Iterate through elements of array
+    for (let i = array.length - 1; i >= 0; i--) {
+        //Iterate through elements of arguments
+        for (let j = newArg.length; j >= 0; j--) {
+            //If element matches argument then remove it from the array
+            if (array[i] === newArg[j]) {
+                array.splice(i,1);
+            }
         }
     }
     return array;
+    
 };
+
+removeFromArray([1,2,3,4],2,3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
-
-
-//turn arguments into array using Array.from()
-
-
-//then run previous function through all elements of arguments array except the 1st
