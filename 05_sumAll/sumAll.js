8ae872ad7e586155688138476db6333054924a12
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-19 18:12:51 +0100

    Completed exercise #5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let finalSum = 0;
+    console.log(Number.isNaN(last));
+    if (first < 0 || last < 0) {
+        return "ERROR";
+    } else if (typeof first != "number" || typeof last !=  "number") {
+        return "ERROR";
+    } else if (first < last) {
+        for (i = last; i >= first; i--) {
+            finalSum += i;
+        }
+        return finalSum;
+    } else {
+        for (i = first; i >= last; i--) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
 
+    
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+
+//create variable to hold final sum
+//loop through each number between and including the 2 given numbers
+//for each number add it to the final sum variable
