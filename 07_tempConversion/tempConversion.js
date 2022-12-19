fe08f1ca0a2ad689221dc96f84a52b822eb675da
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-19 18:42:51 +0100

    Completed exercise #7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let conversion = ((fahrenheit - 32) * (5/9));
+  if (conversion % 1 === 0) return conversion;
+  return Number(conversion.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let conversion = (celsius * (9/5) + 32);
+  if (conversion % 1 === 0) return conversion;
+  return Number(conversion.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +16,14 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+
+//fahrenheit to celsius = (x - 32) * 5/9
+
+//celsius to fahrenheit = (x * 9/5 + 32)
+
+
+
+//take input and run through equation
+//round number to 1 decimal point (num.toFixed(1))
